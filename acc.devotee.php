57a01a75706d322baf19f7a23a78f7ccc1bff62c
Aaron Kuzemchak <aaron@kuzemchak.net>
2011-10-07 16:14:40 -0400

    Added caching

--- acc.devotee.php
+++ acc.devotee.php
@@ -32,11 +32,33 @@ class Devotee_acc {
 	protected $_addons = array();
 	
 	/**
+	 * @var		string
+	 * @access	protected
+	 */
+	protected $_cache_path;
+	
+	/**
+	 * @var		int
+	 * @access	protected
+	 */
+	protected $_cache_time;
+	
+	/**
 	 * Constructor
 	 */
 	public function __construct()
 	{
 		$this->EE =& get_instance();
+		
+		// set cache settings
+		$this->_cache_path = APPPATH.'cache/devotee/';
+		$this->_cache_time = 60*15; // 15 minutes
+		
+		// create cache folder if it doesn't exist
+		if(!is_dir($this->_cache_path))
+		{
+			mkdir($this->_cache_path);
+		}
 	}
 	
 	/**
@@ -79,98 +101,112 @@ class Devotee_acc {
 	 */
 	protected function _get_addons()
 	{
-		$this->EE->load->helper('directory');
-		$this->EE->load->library('addons');
-		$this->EE->load->model('addons_model');
-		$this->EE->load->library('api');
+		// cache file
+		$cache_file = $this->_cache_path.'addons';
 		
-		// scan third_party folder
-		$map = directory_map(PATH_THIRD, 2);
-		
-		// return if nothing found
-		if($map === FALSE)
+		// if cache is still good, use it
+		if(file_exists($cache_file) AND filemtime($cache_file) > (time() - $this->_cache_time))
 		{
-			return 'No third-party addons were found.';
+			$updates = @file_get_contents($cache_file);
 		}
-		
-		// get fieldtypes because the addons library doesn't give all the info
-		$this->EE->api->instantiate('channel_fields');
-		$fieldtypes = $this->EE->api_channel_fields->fetch_all_fieldtypes();
-		
-		// set third-party addons
-		$third_party = array_intersect_key($this->EE->addons->_packages, $map);
-		
-		// get all installed addons
-		$installed = array(
-			'modules' => $this->EE->addons->get_installed('modules'),
-			'plugins' => $this->EE->addons_model->get_plugins(),
-			'extensions' => $this->EE->addons->get_installed('extensions'),
-			'fieldtypes' => $this->EE->addons->get_installed('fieldtypes'),
-			'accessories' => $this->EE->addons->get_installed('accessories')
-		);
-		
-		foreach($third_party as $package => $types)
+		// fetch new content if cache expired
+		else
 		{
-			if(array_key_exists($package, $this->_addons))
+			$this->EE->load->helper('directory');
+			$this->EE->load->library('addons');
+			$this->EE->load->model('addons_model');
+			$this->EE->load->library('api');
+			
+			// scan third_party folder
+			$map = directory_map(PATH_THIRD, 2);
+			
+			// return if nothing found
+			if($map === FALSE)
 			{
-				continue;
+				return 'No third-party addons were found.';
 			}
 			
-			// check for module
-			if(array_key_exists($package, $installed['modules']))
+			// get fieldtypes because the addons library doesn't give all the info
+			$this->EE->api->instantiate('channel_fields');
+			$fieldtypes = $this->EE->api_channel_fields->fetch_all_fieldtypes();
+			
+			// set third-party addons
+			$third_party = array_intersect_key($this->EE->addons->_packages, $map);
+			
+			// get all installed addons
+			$installed = array(
+				'modules' => $this->EE->addons->get_installed('modules'),
+				'plugins' => $this->EE->addons_model->get_plugins(),
+				'extensions' => $this->EE->addons->get_installed('extensions'),
+				'fieldtypes' => $this->EE->addons->get_installed('fieldtypes'),
+				'accessories' => $this->EE->addons->get_installed('accessories')
+			);
+			
+			foreach($third_party as $package => $types)
 			{
-				$addon = $installed['modules'][$package];
-				$this->_set_addon_info($package, $addon['name'], $addon['module_version'], $types);
-			}
-			// check for plugin
-			elseif(array_key_exists($package, $installed['plugins']))
-			{
-				$addon = $installed['plugins'][$package];
-				$this->_set_addon_info($package, $addon['pi_name'], $addon['pi_version'], $types);
-			}
-			// check for extension
-			elseif(array_key_exists($package, $installed['extensions']))
-			{
-				$addon = $installed['extensions'][$package];
-				$this->_set_addon_info($package, $addon['name'], $addon['version'], $types);
-			}
-			// check for fieldtype
-			elseif(array_key_exists($package, $installed['fieldtypes']))
-			{
-				$addon = $fieldtypes[$package];
-				$this->_set_addon_info($package, $addon['name'], $addon['version'], $types);
-			}
-			// check for accessory
-			elseif(array_key_exists($package, $installed['accessories']))
-			{
-				$addon = $installed['accessories'][$package];
-				
-				// we need to load the class if not devotee to get more info
-				if($package != 'devotee')
+				if(array_key_exists($package, $this->_addons))
 				{
-					$acc_path = PATH_THIRD.strtolower($package).'/';
-					$this->EE->load->add_package_path($acc_path, FALSE);
-					$acc = new $addon['class']();
-					$this->EE->load->remove_package_path($acc_path);
-				}
-				// if devotee accessory, we already have the info!
-				else
-				{
-					$acc = array(
-						'name' => $this->name,
-						'version' => $this->version
-					);
-					$acc = (object) $acc;
+					continue;
 				}
 				
-				$this->_set_addon_info($package, $acc->name, $acc->version, $types);
+				// check for module
+				if(array_key_exists($package, $installed['modules']))
+				{
+					$addon = $installed['modules'][$package];
+					$this->_set_addon_info($package, $addon['name'], $addon['module_version'], $types);
+				}
+				// check for plugin
+				elseif(array_key_exists($package, $installed['plugins']))
+				{
+					$addon = $installed['plugins'][$package];
+					$this->_set_addon_info($package, $addon['pi_name'], $addon['pi_version'], $types);
+				}
+				// check for extension
+				elseif(array_key_exists($package, $installed['extensions']))
+				{
+					$addon = $installed['extensions'][$package];
+					$this->_set_addon_info($package, $addon['name'], $addon['version'], $types);
+				}
+				// check for fieldtype
+				elseif(array_key_exists($package, $installed['fieldtypes']))
+				{
+					$addon = $fieldtypes[$package];
+					$this->_set_addon_info($package, $addon['name'], $addon['version'], $types);
+				}
+				// check for accessory
+				elseif(array_key_exists($package, $installed['accessories']))
+				{
+					$addon = $installed['accessories'][$package];
+					
+					// we need to load the class if not devotee to get more info
+					if($package != 'devotee')
+					{
+						$acc_path = PATH_THIRD.strtolower($package).'/';
+						$this->EE->load->add_package_path($acc_path, FALSE);
+						$acc = new $addon['class']();
+						$this->EE->load->remove_package_path($acc_path);
+					}
+					// if devotee accessory, we already have the info!
+					else
+					{
+						$acc = array(
+							'name' => $this->name,
+							'version' => $this->version
+						);
+						$acc = (object) $acc;
+					}
+					
+					$this->_set_addon_info($package, $acc->name, $acc->version, $types);
+				}
 			}
+			
+			$updates = $this->_get_updates();
+			
+			@file_put_contents($cache_file, $updates);
 		}
 		
-		$updates = $this->_get_updates();
-		
 		// return view
-		return $this->EE->load->view('updates', array('updates' => $updates), TRUE);
+		return $this->EE->load->view('updates', array('updates' => json_decode($updates)), TRUE);
 	}
 	
 	/**
@@ -195,7 +231,7 @@ class Devotee_acc {
 	 * Get update info from API
 	 *
 	 * @access	protected
-	 * @return	array|string
+	 * @return	string
 	 */
 	protected function _get_updates()
 	{
@@ -214,8 +250,6 @@ class Devotee_acc {
 		));
 		$response = curl_exec($ch);
 		curl_close($ch);
-		
-		$response = json_decode($response);
 		
 		return $response;
 	}
